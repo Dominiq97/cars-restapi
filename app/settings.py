9917c9bbb214cdcdf378e4434311a4afa2f39eb1
Gigel Babliuc <catanadominic@gmail.com>
2021-09-29 18:36:09 +0300

    deploying

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
-DEBUG = int(os.environ.get('DEBUG', default=0))
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cars-restapi.herokuapp.com']
 
